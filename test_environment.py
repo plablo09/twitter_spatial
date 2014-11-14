31b7b8ea5fc95fcc6262aff609f87a54517813c6
plablo09 <pablo.lopez@centrogeo.edu.mx>
2014-11-14 13:49:06 -0600

    archivo para probar el environmet

---
+++ test_environment.py
@@ -0,0 +1,8 @@
+import pyGDsandbox
+from kde_maps import kde_grid
+import numpy as np
+import matplotlib.pyplot as plt
+import pysal as ps
+from pysal.contrib.viz import mapping as maps
+
+print'yes'
