bbce1b7c4badbd683ea6ffc39fc9ad92f0e7be49
plablo09 <pablo.lopez@centrogeo.edu.mx>
2014-11-26 13:53:36 -0600

    mapa base y ejes uniformes

--- process_files.py
+++ process_files.py
@@ -5,12 +5,13 @@ import matplotlib.pyplot as plt
 import pysal as ps
 import glob
 import ntpath
-
+from pysal.contrib.viz import mapping as maps
 
 
 files = glob.glob('data/dia/*.shp')
 n_files = len(files)
 i = 0
+shp = ps.open('data/distritos.shp')
 
 # for file in files:
 #     print ntpath.basename(file).split(".",1)[0]
@@ -26,18 +27,30 @@ for file in files:
     z = xyz[:, 2].reshape(gdim)
 
     levels = np.linspace(0, z.max(), 25)
-    f = plt.figure(figsize=(8, 4))
+    f = plt.figure(figsize=(14, 4))
 
     ax1 = f.add_subplot(121)
+    base = maps.map_poly_shp(shp)
+    base.set_facecolor('none')
+    base.set_edgecolor('0.5')
+    ax1 = maps.setup_ax([base], ax=ax1)
     ax1.contourf(x, y, z, levels=levels, cmap=plt.cm.bone)
     ax1.set_title("KDE")
+    plt.xlim(460000,520000)
+    plt.ylim(2100000,2210000)
     ax1.xaxis.set_visible(False)
     ax1.yaxis.set_visible(False)
 
     ax2 = f.add_subplot(122)
+    base = maps.map_poly_shp(shp)
+    base.set_facecolor('none')
+    base.set_edgecolor('0.5')
+    ax2 = maps.setup_ax([base], ax=ax2)
     ax2.scatter(points[:, 0], points[:, 1])
     ax2.set_title("Original points")
     ax2.xaxis.set_visible(False)
     ax2.yaxis.set_visible(False)
+    plt.xlim(460000,520000)
+    plt.ylim(2100000,2210000)
     plt.savefig('data/output_tiffs/'+ output_file + '.png')
     i+=1
