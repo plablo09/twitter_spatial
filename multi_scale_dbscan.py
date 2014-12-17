ab19f3ac8f9c7b07e7f64de3584b85ed01fdb033
plablo <plablo09@gmail.com>
2014-12-16 18:11:45 -0600

    Documentación y limpieza
    Traducción al inglés, preparándo el código para el nuevo repositorio.
    Algunos otros arreglos en la forma de pasar los parámetros

--- multi_scale_dbscan.py
+++ multi_scale_dbscan.py
@@ -7,75 +7,83 @@ import numpy as np
 import ogr
 
 
+def compute_dbscan(points,eps, min_samples):
+   """Sklearn DBSCAN wrapper.
 
-class ClusterTree:
+   :param points: a (N,2) numpy array containing the obsevations to cluster
+   :param eps: float to be passed as eps to sklearn.cluster.DBSCAN
+   :param min_samples: int to be passed as min_samples to sklearn.cluster.DBSCAN
+   :returns list with numpy arrays for all the clusters obtained
 
-    def __init__(self,points):
-        self.key = points
-        self.children = []
+   """
+   #Standarize sample
+   scaler = StandardScaler()
+   points = scaler.fit_transform(points)
+   # Compute DBSCAN
+   db = DBSCAN(eps=eps, min_samples=min_samples).fit(points)
+   core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+   core_samples_mask[db.core_sample_indices_] = True
+   labels = db.labels_
+   # Number of clusters in labels, ignoring noise if present.
+   n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+   #Obtain arrays for each cluster except for noise
+   unique_labels = set(labels)
+   points = scaler.inverse_transform(points)
+   clusters = []
+   for l in unique_labels:
+     if l != -1:
+         class_member_mask = (labels == l)
+         clusters.append(points[class_member_mask])
+         #print "cluster size: " + str(len(points[class_member_mask]))
 
-    def append_child(self,child):
-        self.children.append(ClusterTree(child))
-
-
-def compute_dbscan(points,eps, inverse = None):
-    #Standarize sample
-    print "compute_dbscan: recibí %s puntos" % len(points)
-    print "compute_dbscan: el eps es de %s" % eps
-    scaler = StandardScaler()
-    points = scaler.fit_transform(points)
-    # Compute DBSCAN
-    db = DBSCAN(eps=eps, min_samples=10).fit(points)
-    core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
-    core_samples_mask[db.core_sample_indices_] = True
-    labels = db.labels_
-    # Number of clusters in labels, ignoring noise if present.
-    n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
-    #Obtain arrays for each cluster except for noise
-    unique_labels = set(labels)
-    points = scaler.inverse_transform(points)
-    clusters = []
-    for l in unique_labels:
-        if l != -1:
-            class_member_mask = (labels == l)
-            clusters.append(points[class_member_mask])
-            #print "cluster size: " + str(len(points[class_member_mask]))
-
-    print "estoy mandando estos clusters: " + str(len(clusters))
-    return clusters
+   return clusters
 
 cluster_tree = {}
 flat_list = []
 
-def hierarchical_dbscan(cluster_list,eps):
+def hierarchical_dbscan(cluster_list,eps,min_samples,level=None):
+   """
+   Calls compute_dbscan with the specified parameters for each point array
+   in cluster_list
 
-    this_level = []
-    processed = 0
-    tmp = None
-    for cluster in cluster_list:
-        if len(cluster) > 100:
-            tmp = compute_dbscan(cluster,eps)
-            this_level.append(tmp)
-            cluster_tree[cluster.tostring()] = tmp
-            flat_list.append(tmp)
-            processed += 1
-            print "hierarchical_dbscan: estoy guardando %s clusters en el dic" % len(tmp)
+   :param cluster_list: list of (N,2) numpy arrays
+   :param eps: float to be passed as eps to sklearn.cluster.DBSCAN
+   :param min_samples: int to be passed as min_samples to sklearn.cluster.DBSCAN
+   :param level(optional): the depth in the cluster hierarchy
+                        if None then 0-depth is implied
+   """
 
-    print "hierarchical_dbscan: procesé %s clusters en este nivel" % processed
-    if processed > 0:
-        for group in this_level:
-            hierarchical_dbscan(group,eps/2.0)
+   if level == None:
+      level = 0
 
-    else:
-        print 'done'
-        return 'done'
+   this_level = []
+   processed = 0
+   tmp = None
+   for cluster in cluster_list:
+     if len(cluster) > 100:
+         tmp = compute_dbscan(cluster,eps,min_samples)
+         this_level.append(tmp)
+         cluster_tree[cluster.tostring()] = tmp
+         flat_list.append(tmp)
+         processed += 1
+
+   print ("hierarchical_dbscan: %s clusters clusters processed at level %s"
+         % (processed, level))
+   level += 1
+   if processed > 0:
+      for group in this_level:
+         hierarchical_dbscan(group,eps/2.0,min_samples,level)
+
+   else:
+     print 'done'
+     return
 
 
 
 
-# Read data from shape file
+# Read data from shape file and build points array
 driver = ogr.GetDriverByName("ESRI Shapefile")
-ds = driver.Open("data/dia/t_7.shp", 0)
+ds = driver.Open("data/dia/d_0_9.shp", 0)
 l = ds.GetLayer()
 points = []
 for f in l:
@@ -83,11 +91,13 @@ for f in l:
     points.append((g.GetX(),g.GetY()))
 
 points = [np.array(points)]
-hierarchical_dbscan(points,0.8)
-la_ultima = flat_list[len(flat_list)-1]
+
+# Call hierarchical_dbscan
+hierarchical_dbscan(points,0.8,10)
+
+# Plot results
 colors = plt.cm.Spectral(np.linspace(0, 1, len(flat_list)))
 for k, col in enumerate(colors):
-
     level = np.concatenate(flat_list[k])
     label = 'level ' + str(k)
     plt.plot(level[:, 0], level[:, 1], 'o', markerfacecolor=col,
@@ -95,15 +105,3 @@ for k, col in enumerate(colors):
 
 plt.legend(loc='lower right',numpoints=1)
 plt.show()
-
-
-
-
-# points = np.array(points)
-# scaler = StandardScaler()
-# points = scaler.fit_transform(points)
-# points = scaler.inverse_transform(points)
-# plt.plot(points[:, 0], points[:, 1], 'o', markerfacecolor='k',
-#               markeredgecolor='k', markersize=6)
-# plt.show()
-#
